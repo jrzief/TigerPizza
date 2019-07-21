1d0c2b65ea18146299ea4787a943cfdde828ec67
Joel <joelrzief@gmail.com>
2019-07-21 18:34:09 -0400

    Add CSS Transitiond on hover

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -1,30 +1,45 @@
-export const foods = [
+export const foodItems = [
   {
     name: "Favorites",
-    img: "/img/favorites.jpg"
+    img: "/img/favorites.jpg",
+    section: "Pizza"
   },
   {
     name: "The Lineup",
-    img: "/img/the-lineup.jpg"
+    img: "/img/the-lineup.jpg",
+    section: "Sides"
   },
   {
     name: "Cheese-Onions-Mushrooms",
-    img: "/img/pizza6.jpeg"
+    img: "/img/pizza6.jpeg",
+    section: "Pizza"
   },
   {
     name: "Mixed Peppers",
-    img: "/img/princeton-pi3.jpg"
+    img: "/img/princeton-pi3.jpg",
+    section: "Pizza"
   },
   {
     name: "Cheese-Eggs-&More",
-    img: "/img/cheese-eggs-more.jpg"
+    img: "/img/cheese-eggs-more.jpg",
+    section: "Pizza"
   },
   {
     name: "Basil-Cheese-Chili-Peppers",
-    img: "/img/basil-cheese-chili-peppers.jpg"
+    img: "/img/basil-cheese-chili-peppers.jpg",
+    section: "Pizza"
   },
   {
     name: "Black Olives-Corn",
-    img: "/img/black-olives-cheese-corn.jpg"
+    img: "/img/black-olives-cheese-corn.jpg",
+    section: "Pizza"
   }
 ];
+
+export const foods = foodItems.reduce((res, food) => {
+  if (!res[food.section]) {
+    res[food.section] = [];
+  }
+  res[food.section].push(food);
+  return res;
+}, {});
