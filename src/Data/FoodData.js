0ebbb4c9f2ff03a2718189ee4f1dd9cda8484895
Joel <joelrzief@gmail.com>
2019-07-21 17:01:26 -0400

    Add Menu, Food data, FoodGrid

---
+++ src/Data/FoodData.js
@@ -0,0 +1,30 @@
+export const foods = [
+  {
+    name: "Favorites",
+    img: "/img/favorites.jpg"
+  },
+  {
+    name: "The Lineup",
+    img: "/img/the-lineup.jpg"
+  },
+  {
+    name: "Cheese-Onions-Mushrooms",
+    img: "/img/pizza6.jpeg"
+  },
+  {
+    name: "Mixed Peppers",
+    img: "/img/princeton-pi3.jpg"
+  },
+  {
+    name: "Cheese-Eggs-&More",
+    img: "/img/cheese-eggs-more.jpg"
+  },
+  {
+    name: "Basil-Cheese-Chili-Peppers",
+    img: "/img/basil-cheese-chili-peppers.jpg"
+  },
+  {
+    name: "Black Olives-Corn",
+    img: "/img/black-olives-cheese-corn.jpg"
+  }
+];
