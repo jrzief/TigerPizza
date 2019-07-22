9c6f2eb9cf2eae2de0b1d8c92d99d1eb4a95c262
Joel <joelrzief@gmail.com>
2019-07-21 23:14:15 -0400

    Cleanup-refactor

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from "react";
-import { createGlobalStyle } from "styled-components";
 import { Navbar } from "./Navbar/Navbar";
 import {Banner} from './Banner/Banner';
 import {Menu} from './Menu/Menu';
+import {GlobalStyle} from './Styles/GlobalStyle';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    font-family: 'Open Sans', sans-serif;
-  }
-  h1, h2, h3 {
-    font-family: 'Righteous', cursive;
-  }
-`;
+
 
 function App() {
   return (
@@ -21,8 +13,7 @@ function App() {
       <Navbar/>
       <Banner />
       <Menu />
-      <div>Hello Big Slice</div>
-    </>
+    </>  
   );
 }
 
