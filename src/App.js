389f4231f0465f2180426f0a1891bf6ef91ea177
Joel <joelrzief@gmail.com>
2019-07-25 10:11:34 -0400

    Add Food Dialog and Order Footers

--- src/App.js
+++ src/App.js
@@ -4,18 +4,25 @@ import {Banner} from './Banner/Banner';
 import {Menu} from './Menu/Menu';
 import {FoodDialog} from './FoodDialog/FoodDialog';
 import {GlobalStyle} from './Styles/GlobalStyle';
+import {Order} from './Order/Order';
+import { useOpenFood } from './Hooks/useOpenFood';
+import { useOrders } from './Hooks/useOrders';
 
 
 
 function App() {
- const [openFood, setOpenFood] =  useState();
+ //const [openFood, setOpenFood] =  useState();
+ const openFood = useOpenFood();
+ const orders = useOrders();
+
   return (
     <>
       <GlobalStyle />
-      <FoodDialog openFood={openFood} setOpenFood={setOpenFood}/>
+      <FoodDialog {...openFood} {...orders}/>
       <Navbar/>
+      <Order {...orders}/>
       <Banner />
-      <Menu setOpenFood={setOpenFood} />
+      <Menu {...openFood} />
     </>  
   );
 }
--- src/FoodDialog/FoodDialog.js
+++ src/FoodDialog/FoodDialog.js
@@ -1,17 +1,45 @@
 import React from "react";
 import styled from "styled-components";
 import {FoodLabel} from '../Menu/FoodGrid';
+import {pizzaRed} from '../Styles/colors';
+import {Title} from '../Styles/title';
 
 export const Dialog = styled.div`
   width: 500px;
-  height: 500px;
   background-color: white;
   position: fixed;
   top: 75px;
   z-index: 5;
-  max-height: calc(1000% - 100px);
+  max-height: calc(100% - 100px);
   left: calc(50% - 250px);
+  display: flex;
+  flex-direction: column;
   
+`;
+
+
+export const DialogContent = styled.div`
+  overflow: auto;
+  min-height: 100px;
+`;
+
+export const DialogFooter = styled.div`
+  box-shadow: 0px -2px 10px 0px grey;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const ConfirmButton = styled(Title)`
+  margin: 10px;
+  color: white;
+  height: 20px;
+  border-radius: 5px;
+  padding: 10px;
+  width: 200px;
+  background-color: ${pizzaRed};
+  text-align: center;
+
 `;
 
 export const DialogShadow = styled.div`
@@ -51,6 +79,11 @@ export function FoodDialog({openFood, setOpenFood})  {
                         <DialogBanner img={openFood.img}>
                            <DialogBannerName>{openFood.name}</DialogBannerName>
                         </DialogBanner>
+                        <DialogContent>
+                        </DialogContent>
+                        <DialogFooter>
+                           <ConfirmButton>Add to Order</ConfirmButton>
+                        </DialogFooter>
                     </Dialog>
            </>
       
